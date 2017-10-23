ffd69ae2e5be216e6c8db912eb4e54663ea884ad
Luke Keltner <lkeltner87@gmail.com>
2017-10-23 18:17:05 -0400

    trying AGAIN

--- config/connection.js
+++ config/connection.js
@@ -1,14 +1,13 @@
 var mysql = require("mysql");
-var connection;
-/* = mysql.createConnection(
+var connection = mysql.createConnection(
 {
 	host: "otmaa16c1i9nwrek.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
 	user: "aqlmqbpyrbwx9fud",
 	password: "aiexiw6aq63h68tm",
 	database: "bqzftpqzzcha0u20"
 });
-*/
-if (process.env.JawsDB_URL)
+
+/*if (process.env.JawsDB_URL)
 {
 	connection = mysql.createConnection(process.env.JAWSDB_URL);
 }
@@ -24,7 +23,7 @@ else
 		password: 'Lukkehoday1',
 		database: "burgers_db"
 	});
-};
+};*/
 
 connection.connect(function(err)
 {
