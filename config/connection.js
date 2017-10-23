60ad66ccc0cc1be98a380c070eb27fa8ef9078f8
Luke Keltner <lkeltner87@gmail.com>
2017-10-23 17:42:19 -0400

    adding to heroku

--- config/connection.js
+++ config/connection.js
@@ -9,6 +9,22 @@ var connection = mysql.createConnection(
 	database: "burgers_db"
 });
 
+if (process.env.JawsDB_URL)
+{
+	connection = mysql.createConnection(process.env.JAWSDB_URL);
+}
+
+else
+{
+	connection = mysql.createConnection(
+	{
+		host: "localhost",
+		user: 'root',
+		password: 'Lukkehoday1',
+		database: "burgers_db"
+	});
+};
+
 connection.connect(function(err)
 {
 	if(err){throw err}
